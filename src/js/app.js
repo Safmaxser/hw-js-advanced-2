6159e86a7b246030c95b53fe99aa2fcbbce88ff4
Safmaxser <md-86@mail.ru>
2025-01-21 20:28:17 +0700

    improvement

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
 // TODO: write your code here
 export function displaySpecialAttack(character) {
-  const newListSpecial = [];
-  character.special.forEach(elSpecial => {
-    const { description = 'Описание недоступно', ...data } = elSpecial;
-    newListSpecial.push({...data, description})
+  return character.special.map(({ description = 'Описание недоступно', ...data }) => {
+    return {...data, description}
   });
-  return newListSpecial;
 }
